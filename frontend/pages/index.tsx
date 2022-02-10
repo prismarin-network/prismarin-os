9530920f329375bf5bfaa36270a54e687265f718
ReaperMaga <reapermaga@gmail.com>
2022-02-10 14:12:52 +0100

    Moved Loading to a component file, added new loading animation

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,37 +3,13 @@ import Layout from "../layouts/Layout";
 import styles from "../styles/modules/gradients.module.css"
 import Logo from "../components/common/media/Logo";
 import {Transition} from "@headlessui/react";
+import Loading from "../components/Loading";
 
 const IndexPage = observer(() => {
 
     return (
         <Layout>
-            <Transition
-                show={true}
-                appear={true}
-                enter="transition-opacity duration-700"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="transition-opacity duration-150"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-            >
-                <div className="relative w-full h-screen">
-                    <div className={`absolute w-full h-full ${styles.lightBlueGradient}`}/>
-                    <div className="flex absolute flex-col justify-center items-center w-full h-full text-center">
-                        <Transition.Child
-                            enter="transition-opacity duration-700 delay-700"
-                            enterFrom="opacity-0"
-                            enterTo="opacity-100"
-                        >
-                            <Logo height={150} className="mx-auto fill-blue-100"/>
-                            <div className="my-4 text-lg font-bold text-blue-100 uppercase">
-                                Click anywhere to continue...
-                            </div>
-                        </Transition.Child>
-                    </div>
-                </div>
-            </Transition>
+            <Loading />
         </Layout>
     )
 })

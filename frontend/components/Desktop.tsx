b45c5ac990f1fbafbcdd79df07f9dbc053e4328d
ReaperMaga <reapermaga@gmail.com>
2022-02-12 13:13:49 +0100

    Removed Others component

--- frontend/components/Desktop.tsx
+++ frontend/components/Desktop.tsx
@@ -1,6 +1,3 @@
-import desktopStore from "../stores/DesktopStore";
-import Icon from "./Icon";
-import Window from "./Window";
 import styles from "../styles/modules/gradients.module.css";
 import ApplicationBar from "./ApplicationBar";
 
@@ -19,22 +16,5 @@ const Desktop = () => {
     )
 }
 
-
-const Others = () => {
-    return (
-        <>
-            {Object.entries(desktopStore.icons).map(([, icon], index) => <Icon key={index} icon={icon} />)}
-
-            <Window
-                id="Home"
-                classes="w-[500px]"
-            >
-                <div className="p-4 h-80 bg-gray-600">
-                    Maga riecht toll! (nach Rosen!)
-                </div>
-            </Window>
-        </>
-    )
-}
 
 export default Desktop

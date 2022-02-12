e7246258e6190efae153a6c5cd91c8efb17d19d6
ReaperMaga <reapermaga@gmail.com>
2022-02-12 13:31:55 +0100

    Removed white icon

--- frontend/components/ApplicationBar.tsx
+++ frontend/components/ApplicationBar.tsx
@@ -1,10 +1,8 @@
 const ApplicationBar = () => {
     return (
-        <div className="p-5 w-1/2 h-28 rounded-t-2xl bg-zinc-700/50">
+        <div className="p-5 w-1/2 h-28 rounded-t-2xl shadow bg-zinc-700/50">
             <div className="grid grid-cols-10 gap-5 h-full">
-                <div className="bg-white">
 
-                </div>
             </div>
         </div>
     )
